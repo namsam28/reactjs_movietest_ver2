05f35c814d34f2ca6bf58513564eb840b96db9d2
namsam28 <namsam275@gmail.com>
2020-06-09 22:53:24 +0900

    Updates

---
+++ precache-manifest.db1f05fe40d7ef500b851bd13acdacb4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d73cb0e9d3ad059e5e6f4a9b52f5070",
+    "url": "/reactjs_movietest_ver2/index.html"
+  },
+  {
+    "revision": "21c5cbf80153206c10d8",
+    "url": "/reactjs_movietest_ver2/static/css/main.5bd1f2cd.chunk.css"
+  },
+  {
+    "revision": "2dca63e70ba2a9074624",
+    "url": "/reactjs_movietest_ver2/static/js/2.dc4e425c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactjs_movietest_ver2/static/js/2.dc4e425c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21c5cbf80153206c10d8",
+    "url": "/reactjs_movietest_ver2/static/js/main.9026555a.chunk.js"
+  },
+  {
+    "revision": "f79e5fe113a074b6d9fb",
+    "url": "/reactjs_movietest_ver2/static/js/runtime-main.b4aecb6f.js"
+  }
+]);
