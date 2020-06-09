40cf2fa152b4df6ae50ff07ebc71c28d8a4f1891
namsam28 <namsam275@gmail.com>
2020-06-09 11:44:40 +0900

    life cycle study

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
 import React from 'react';
+//import PropTypes from 'prop-types';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends React.Component{
+	constructor(props){
+		super(props);
+		console.log("hello!");
+	}
+
+	state = {
+		count : 0
+	};
+	add = () => {
+		console.log('add');
+		//this.setState({ count : this.state.count + 1 });
+		this.setState(current => ({ 
+			count : current.count + 1
+		}));
+	};
+	minus = () => {
+		console.log('minus');
+		//this.setState({ count : this.state.count - 1 });
+		this.setState(current => ({ 
+			count : current.count - 1 
+		}));
+	};
+
+	componentDidMount(){
+		console.log('Component rendered!')
+	};
+
+	componentDidUpdate(){
+		console.log('Component Update!')
+	};
+
+	render(){
+		console.log("render");
+		return (
+		<div>
+			<h1>The number is {this.state.count}</h1>
+			<button onClick={this.add}>Add</button>
+			<button onClick={this.minus}>Minus</button>
+		</div>);
+	}
 }
 
 export default App;
